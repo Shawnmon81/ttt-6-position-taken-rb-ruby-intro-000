4014f78427f7393bf03ed0ace0c6a8eec6d54bd6
munificent-forum-1695 <bunchshawn@gmail.com>
2020-08-10 17:09:16 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,5 +1,8 @@
 def position_taken?(board, index)
-  if board[index] == " "
+  if board[index] == "X"
+  elsif board[index] == "O"
+  else
+    nil
   end
 end
 # code your #position_taken? method here!
